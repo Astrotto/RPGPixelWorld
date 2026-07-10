eb819108cc7a1b288378b924e288b9bafa51ab42
Mattia D'Arcangelo <mattia.darcangelo@studenti.unicam.it>
2026-07-10 09:13:24 +0200

    Creazione della classe PrintLogDeath per la visualizzazione a schermo della morte di un entità

---
+++ src/main/java/it/unicam/cs/mpgc/rpg129851/Model/DeathSystem.java
@@ -0,0 +1,11 @@
+package it.unicam.cs.mpgc.rpg129851.Model;
+
+public class DeathSystem {
+
+
+    public void deathControl(Entity attacker, Entity defender){
+        if(defender.getHealth().getStatistic() <= 0){
+
+        }
+    }
+}
