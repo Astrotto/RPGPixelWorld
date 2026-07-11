3564abb8de2bf590763b5d2329ad263866ed9f40
Mattia D'Arcangelo <mattia.darcangelo@studenti.unicam.it>
2026-07-11 18:04:40 +0200

    Aggiunto System.exit(0) quando viene cliccato il tasto Chiudi

--- src/main/java/it/unicam/cs/mpgc/rpg129851/Controller/MenuGameController.java
+++ src/main/java/it/unicam/cs/mpgc/rpg129851/Controller/MenuGameController.java
@@ -1,14 +1,20 @@
 package it.unicam.cs.mpgc.rpg129851.Controller;
 
+import it.unicam.cs.mpgc.rpg129851.View.PlayerView;
+import it.unicam.cs.mpgc.rpg129851.View.ViewRegister;
+import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 import static it.unicam.cs.mpgc.rpg129851.ImagesLoader.BackgroundLoader.setBackgroundView;
 import static it.unicam.cs.mpgc.rpg129851.ImagesLoader.ButtonLoader.loadButton;
+import static it.unicam.cs.mpgc.rpg129851.Launch.ChangerMap.changeMap;
+import static it.unicam.cs.mpgc.rpg129851.Launch.ChangerMap.setNewScene;
 import static it.unicam.cs.mpgc.rpg129851.Launch.Main.player;
 
-public class MenuGameController{
+public class MenuGameController {
     @FXML
     private ImageView btnStart, btnQuit;
     @FXML
@@ -33,4 +39,9 @@ public class MenuGameController{
     public void quiGame(){
         System.exit(0);
     }
+    @FXML
+    public void startGame(){
+        changeMap("map");
+    }
+
 }
